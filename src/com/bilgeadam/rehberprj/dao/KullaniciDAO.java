2cddf06c3a435a463ba7a60d173dac7f47041f7a
Emre Aktaş <50225106+kmsync@users.noreply.github.com>
2021-12-26 11:00:35 +0300

    kullanıcı aktif değerini kontrol etmek için kodlar eklendi.

--- src/com/bilgeadam/rehberprj/dao/KullaniciDAO.java
+++ src/com/bilgeadam/rehberprj/dao/KullaniciDAO.java
@@ -1,37 +1,51 @@
 package com.bilgeadam.rehberprj.dao;
 
 import com.bilgeadam.rehberprj.dto.KullaniciDTO;
+import com.bilgeadam.rehberprj.sec.AES;
+import com.bilgeadam.rehberprj.util.Sabitler;
 import com.bilgeadam.rehberprj.vt.VTBaglanti;
 
 import java.sql.*;
 
 public class KullaniciDAO {
 
-    public static boolean giriseYetkilimi(KullaniciDTO kullanici) throws SQLException, ClassNotFoundException {
+    public static boolean giriseYetkilimi(KullaniciDTO kullanici)
+            throws SQLException, ClassNotFoundException {
 
         String vtSifre = null;
 
         Connection conn = VTBaglanti.baglantiGetir();
 
-        String sorgu = "select sifre from kullanici where kullanici_adi=?";
+        String sorgu = "select sifre from kullanici where kullanici_adi=? and aktif=?";
+
         PreparedStatement ps = conn.prepareStatement(sorgu);
 
         ps.setString(1, kullanici.getKullaniciAdi());
+        ps.setInt(2, kullanici.getAktif());
 
         ResultSet rs = ps.executeQuery();
 
-        while (rs.next()) {
+        while(rs.next())
+        {
             vtSifre = rs.getString("sifre");
         }
-
         rs.close();
 
         ps.close();
-
         VTBaglanti.baglantiKapat(conn);
 
-        if (kullanici.getSifre().equals(vtSifre))
+        String cozulmus_sifre = null;
 
+        if(vtSifre != null) {
+            cozulmus_sifre = AES.decrypt(vtSifre, Sabitler.SECRET_KEY);
+        }
+        else{
+            // vtSifre null ise aşağıdaki eşitliği kontrol etmeye gerek yok.
+            // Buradan metod sonucunu false olarak döndürüyorum.
+            return false;
+        }
+
+        if(kullanici.getSifre().equals(cozulmus_sifre))
             return true;
         else
             return false;
